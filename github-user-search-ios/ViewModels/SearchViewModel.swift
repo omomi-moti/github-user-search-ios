9d43546ab4d83fdaf8cc20a990c9f183a0e7df72
Seiya Suzuki <ikpubon@icloud.com>
2026-07-12 00:31:54 +0900

    feat: 検索画面用のSearchViewModelを追加

--- github-user-search-ios/ViewModels/SearchViewModel.swift
+++ github-user-search-ios/ViewModels/SearchViewModel.swift
@@ -1,7 +1,46 @@
-//
-//  SearchViewModel..swift
-//  github-user-search-ios
-//
-//  Created by 鈴木聖也 on 2026/07/11.
-//
+import Foundation
+import Observation
+
+@Observable
+@MainActor
+class SearchViewModel{
+    var keyword : String = ""
+    var state : ViewState<[SearchUser]> = .idle
+    
+    private let repository : GitHubRepository
+    private var searchTask : Task<Void, Never>? //<このTaskが完了した時に、何か値を返すか,このTaskがエラーを投げる可能性があるか>
+    
+    init(repository : GitHubRepository){
+        self.repository = repository
+    }
+    
+    func onKeywordChanged(){
+        searchTask?.cancel()
+        
+        guard !keyword.isEmpty else{ //キーワードの入力がない場合は呼ばれても何もしない
+            state = .idle
+            return
+        }
+        
+        searchTask = Task{
+            try? await Task.sleep(for: .milliseconds(300))
+            guard !Task.isCancelled else{ return }
+            await search()
+        }
+        
+    }
+    private func search() async{
+        state = .loading
+        do{
+            let users = try await repository.searchUsers(keyword: keyword)
+            guard !Task.isCancelled else{ return }
+            state = .loaded(users)
+            
+        }
+        catch{
+            guard !Task.isCancelled else{ return }
+            state = .error("検索に失敗しました")
+        }
+    }
+}
 
