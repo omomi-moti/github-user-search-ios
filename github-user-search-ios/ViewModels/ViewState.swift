031c1c74246ed6982ea2af4d3bfc22672d0f780f
Seiya Suzuki <ikpubon@icloud.com>
2026-07-12 21:47:30 +0900

    fix: ViewStateのコメント誤字を修正しEquatable準拠を追加

--- github-user-search-ios/ViewModels/ViewState.swift
+++ github-user-search-ios/ViewModels/ViewState.swift
@@ -2,7 +2,9 @@ import Foundation
 
 enum ViewState<T>{
     case idle //まだ何もしてない状態
-    case loading //ロード中の除隊
+    case loading //ロード中の状態
     case loaded(T) //データが手元にある状態
     case error(String) //通信が失敗した状態
 }
+
+extension ViewState: Equatable where T: Equatable {}
